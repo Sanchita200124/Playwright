e36c34a61491852a3929ad40fd4532593d25eae4
Sanchita200124 <ssanchu40@gmail.com>
2025-12-07 08:50:59 +0530

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,13 @@
+let firstName="Sanchita Sharma"
+let companyName="XYZ Company123"
+let mobileNumber=990000012
+let isAutomation=true
+let hasPlaywright
+
+console.log(
+    "First Name:       "+firstName+"\t"+" =>Datatype is: "+typeof firstName+"\n"+
+    "Company Name:     "+companyName+"\t"+" =>Datatype is: "+typeof companyName+"\n"+
+    "Mobile Number:    "+mobileNumber+"\t"+" =>Datatype is: "+typeof mobileNumber+"\n"+
+    "Is Automation:    "+isAutomation+"\t"+" =>Datatype is: "+typeof isAutomation+"\n"+
+    "Has Playwright:   "+hasPlaywright+"\t"+" =>Datatype is: "+typeof hasPlaywright+"\n"
+);
